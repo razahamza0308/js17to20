55ede7d850f197d9fb9f32744fb0e723d2c9069c
razahamza0308 <razahamza0308@gmail.com>
2021-05-06 06:52:28 +0500

    raza

---
+++ app.js
@@ -0,0 +1,136 @@
+                    //  **********Question 1************
+
+// var arr=[[]];
+
+
+                    //  **********Question 2************
+
+
+// var array=[["0 1 2 3"],["1 0 1 2"],["2 1 0 1"]];
+// for(var i=0;i<=array.length-1;i++){
+// document.write(array[i]+"<br>");}
+
+
+                    //  **********Question 3************
+
+
+// for(var i=1; i<=10; i++){
+//     document.write(i+"<br>");
+// }
+
+
+                    //  **********Question 4************
+
+
+// var table=prompt("Enter no. for tale",2);
+// var tab=prompt("Enter lenght of the table",10);
+// for(var i=1; i<=tab; i++){
+//     document.write(table+"X"+i+"="+tab*i+"<br>");
+// }
+
+
+                    //  **********Question 5************
+
+
+// var fruits=["apple","banana","mango","orange","strawberry"];
+// for(var i=0; i<=fruits.length-1;i++){
+//     document.write(fruits[i]+"<br>");
+// }
+// for(var i=0; i<=fruits.length-1;i++){
+//     document.write("Element at index "+i+" is "+fruits[i]+"<br>");
+// }
+
+
+                    //  **********Question 6************
+
+
+// var i
+// document.write("<h2>Counting<h2>")
+// for(i=0;i<=15;i++)
+// document.write(i+' , ' );
+
+
+// var i
+// document.write("<h2>Reverse<h2>")
+// for(i=15;i>=0;i--)
+// document.write(i+', ');
+
+
+// var i
+// document.write("<h2>Even<h2>")
+// for(i=0;i<=20;i++)
+// if(i%2===0){
+// document.write(i+' ,')};
+
+
+// var i
+// document.write("<h2>odd<h2>")
+// for(i=0;i<=20;i++)
+// if(i%2===1){
+// document.write(i+' ,')};
+
+
+
+
+
+// var i
+// document.write("<h2>series<h2>")
+// for(i=1;i<=20;i++)
+// if(i%2===0){
+// document.write(i+'k ')};
+
+
+                    //  **********Question 7************
+
+// var arr=['cake', 'apple pie', 'cookie', 'chips', 'patties']
+
+// var userinput=prompt('Welcome to ABC Bakery.What do you want to order sir/maam')
+
+// var flag=false
+
+// for(i=0;i<=arr.length;i++){
+//     if(userinput===arr[i]){
+//         flag=true
+//         document.write(userinput+" is <b>Available</b> "+"at index " + i +" in our Bakery")
+//     }
+
+// }
+
+// if(!flag)
+// {document.write("We are Sorry "+userinput+" is not available in our bakery")}
+
+
+
+
+
+
+
+
+                    //  **********Question 8************
+
+
+// var arrLar=[24,53,78,91,12];
+// var no=Math.max.apply(null, arrLar);
+// document.write(no+" is the largest number"+"<br>");
+
+
+
+
+
+                    //  **********Question 9************
+
+
+// var arrLar=[24,53,78,91,12];
+// var no=Math.min.apply(null, arrLar);
+// document.write(no+" is the smallest number");
+
+
+                    //  **********Question 10************
+
+
+// var arr;
+// for(var i=0; i<=100; i++){
+//     if(i%5==0){
+//         document.write(i+"<br>")
+//     }
+// }
